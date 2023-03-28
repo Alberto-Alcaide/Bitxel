d686b6a46f198315e7c39b00b23658f76bd6f28b
Alberto-Alcaide <alcaidevegaalberto@gmail.com>
2023-03-28 15:49:50 +0200

    Created Tests

---
+++ src/test/java/dao/Missions/MissionsTest.java
@@ -0,0 +1,10 @@
+package dao.Missions;
+
+public class MissionsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/dao/Stats/StatsTest.java
@@ -0,0 +1,10 @@
+package dao.Stats;
+
+public class StatsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
