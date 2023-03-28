c5b0343429eac257eeca1215b2b2b69fc654f304
JuanManuelRider <104312004+JuanManuelRider@users.noreply.github.com>
2023-03-28 16:46:54 +0200

    Missions Test

--- src/main/java/dao/Stats/StatsImpl.java
+++ src/main/java/dao/Stats/StatsImpl.java
@@ -1,5 +1,7 @@
 package dao.Stats;
 
+import java.util.Objects;
+
 public class StatsImpl implements Stats {
 	
 	private Integer salud;
@@ -11,88 +13,99 @@ public class StatsImpl implements Stats {
 	private Integer suerte;
 	
 	
+	/**
+	 * @param salud
+	 * @param velocidad
+	 * @param experiencia
+	 * @param nivel
+	 * @param ataque
+	 * @param defensa
+	 * @param suerte
+	 */
+	public StatsImpl(Integer salud, Integer velocidad, Integer experiencia, Integer nivel, Integer ataque,
+			Integer defensa, Integer suerte) {
+		super();
+		this.salud = salud;
+		this.velocidad = velocidad;
+		this.experiencia = experiencia;
+		this.nivel = nivel;
+		this.ataque = ataque;
+		this.defensa = defensa;
+		this.suerte = suerte;
+	}
+	
 	@Override
+	public String toString() {
+		return "StatsImpl [salud=" + salud + ", velocidad=" + velocidad + ", experiencia=" + experiencia + ", nivel="
+				+ nivel + ", ataque=" + ataque + ", defensa=" + defensa + ", suerte=" + suerte + "]";
+	}
+
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ataque, defensa, experiencia, nivel, salud, suerte, velocidad);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StatsImpl other = (StatsImpl) obj;
+		return Objects.equals(ataque, other.ataque) && Objects.equals(defensa, other.defensa)
+				&& Objects.equals(experiencia, other.experiencia) && Objects.equals(nivel, other.nivel)
+				&& Objects.equals(salud, other.salud) && Objects.equals(suerte, other.suerte)
+				&& Objects.equals(velocidad, other.velocidad);
+	}
+
 	public Integer getSalud() {
-		// TODO Auto-generated method stub
-		return null;
+		return salud;
 	}
-
-	@Override
 	public void setSalud(Integer salud) {
-		// TODO Auto-generated method stub
-
+		this.salud = salud;
 	}
-
-	@Override
 	public Integer getVelocidad() {
-		// TODO Auto-generated method stub
-		return null;
+		return velocidad;
 	}
-
-	@Override
 	public void setVelocidad(Integer velocidad) {
-		// TODO Auto-generated method stub
-
+		this.velocidad = velocidad;
 	}
-
-	@Override
 	public Integer getExperiencia() {
-		// TODO Auto-generated method stub
-		return null;
+		return experiencia;
 	}
-
-	@Override
 	public void setExperiencia(Integer experiencia) {
-		// TODO Auto-generated method stub
-
+		this.experiencia = experiencia;
 	}
-
-	@Override
 	public Integer getNivel() {
-		// TODO Auto-generated method stub
-		return null;
+		return nivel;
 	}
-
-	@Override
 	public void setNivel(Integer nivel) {
-		// TODO Auto-generated method stub
-
+		this.nivel = nivel;
 	}
-
-	@Override
 	public Integer getAtaque() {
-		// TODO Auto-generated method stub
-		return null;
+		return ataque;
 	}
-
-	@Override
-	public void setAtaque(Integer Ataque) {
-		// TODO Auto-generated method stub
-
+	public void setAtaque(Integer ataque) {
+		this.ataque = ataque;
 	}
-
-	@Override
 	public Integer getDefensa() {
-		// TODO Auto-generated method stub
-		return null;
+		return defensa;
 	}
-
-	@Override
-	public void setDefensa(Integer Defensa) {
-		// TODO Auto-generated method stub
-
+	public void setDefensa(Integer defensa) {
+		this.defensa = defensa;
 	}
-
-	@Override
 	public Integer getSuerte() {
-		// TODO Auto-generated method stub
-		return null;
+		return suerte;
 	}
-
-	@Override
-	public void setSuerte(Integer Suerte) {
-		// TODO Auto-generated method stub
-
+	public void setSuerte(Integer suerte) {
+		this.suerte = suerte;
 	}
-
+	
+	
+	
 }
